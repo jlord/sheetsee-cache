681bb6d38c6b0eef268f68b8c91fe15886498d7a
jllord <jessica@codeforamerica.org>
2013-04-21 20:08:34 -0700

    got bar chart sorting to work

--- js/sheetsee.js
+++ js/sheetsee.js
@@ -103,6 +103,8 @@ function addPopups() {
 // D3 Bar Chart
 //
 function renderGraph(data, noOfItems, divTown) {
+  console.log(data)
+
 //  m = [t0, r1, b2, l3]
 var m = [30, 60, 10, 200],
     w = 600 - m[1] - m[3],
@@ -123,11 +125,12 @@ var svg = d3.select(divTown).append("svg")
     .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
   // Parse numbers, and sort by value.
-  data.forEach(function(d) { d.units = +d.units; });
+  // data.forEach(function(d) { d.units = +d.units; });
+ //  data.sort(function(a, b) { return b.units - a.units; });
 
   // Set the scale domain.
-  x.domain([0, d3.max(data, function(d) { return d.units; })]);
-  y.domain(data.map(function(d) { return d.label; }));
+  x.domain([0, d3.max(data, function(d) { return d.units; })]); // 0 to max of units
+  y.domain(data.map(function(d) { return d.label; })); // makes array of labels
 
   var bar = svg.selectAll("g.bar")
     .data(data)
@@ -156,4 +159,76 @@ var svg = d3.select(divTown).append("svg")
   svg.append("g")
     .attr("class", "y axis")
     .call(yAxis);
+
+// return chartInfo = {"data": data, "y": y, "bar": bar}
+
+d3.select("input").on("change", change);
+
+// var sortTimeout = setTimeout(function() {
+//   d3.select("input").property("checked", true).each(change);
+// }, 2000);
+
+  function change() {
+    // clearTimeout(sortTimeout);
+
+    // Copy-on-write since tweens are evaluated after a delay.
+    var y0 = y.domain(data.sort(this.checked
+        ? function(a, b) { return b.units - a.units; }
+        : function(a, b) { return d3.ascending(a.label, b.label); })
+        .map(function(d) { return d.label; }))
+        .copy();
+
+    var transition = svg.transition().duration(750),
+        delay = function(d, i) { return i * 50; };
+
+    transition.selectAll(".bar")
+        .delay(delay)
+        .attr("transform", function(d) { return "translate(0," + y(d.label) + ")"; });
+
+    transition.select(".y.axis")
+        .call(yAxis)
+      .selectAll("g")
+        .delay(delay);
+  }
+
+
+
+
 };
+
+
+
+
+
+
+
+
+// function sortChart(chartInfo) {
+//   console.log('hey why')
+//   chartInfo.data.forEach(function(d) { d.units = +d.units; });
+//   chartInfo.data.sort(function(a, b) { return b.units - a.units; });
+//   console.log("chartinfodata is", chartInfo.data)
+//  // chartInfo.y.domain(chartInfo.data.map(function(d) { return d.label; })); 
+
+//   chartInfo.bar.transition()
+//       .duration(1000)
+//       .delay(function(d, i) { return i * 50; })
+//       // .attr("transform", function(d, i) { return "translate(0," + chartInfo.y(d.label, i) + ")"; })
+//       .attr("transform", function(d, i) { return "translate(0," + chartInfo.y(d.units, i) + ")"; });
+
+//   return  chartInfo
+// }
+
+// function sortChart() {
+//     index.sort(function(a, b) { return data[a] - data[b]; });
+
+//   y.domain(index);
+
+//   bar.transition()
+//       .duration(750)
+//       .delay(function(d, i) { return i * 50; })
+//       .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
+
+// }
+
+
