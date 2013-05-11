6258867b6324b05c464e1db367f0108bb297209e
jllord <jessica@codeforamerica.org>
2013-05-11 16:04:43 -0700

    data swap

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ var router = require('router')
 // globals
 var sheetData = []
 var lastFetch 
-var KEY = '0Ao5u1U6KYND7dGJpT3cxTGZrZS1WdWo5RnNpa1dZQmc'
+var KEY = '0Ao5u1U6KYND7dGN5QngweVJUWE16bTRob0d2a3dCbnc'
 
-// https://docs.google.com/spreadsheet/pub?key=0Ao5u1U6KYND7dGJpT3cxTGZrZS1WdWo5RnNpa1dZQmc&output=html
+
+// 0Ao5u1U6KYND7dGN5QngweVJUWE16bTRob0d2a3dCbnc has lat/long
+// cool
+// 0Ao5u1U6KYND7dGJpT3cxTGZrZS1WdWo5RnNpa1dZQmc removed lat/long 
+// for geocoding testing
 
 // ready, set, go!
 function reqHandler (req, res) {
